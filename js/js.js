104dd9dce863cc57191a41ee39e677d2ee35a1e5
ESRAAmamdoh <emamdoh144@gmail.com>
2023-12-11 19:05:11 +0200

    first commit

---
+++ js/js.js
@@ -0,0 +1,60 @@
+
+var quotes = [
+    {
+        quote: "Do not pity the dead, Harry. Pity the living, and, above all those who live without love.",
+        author: "Albus Dumbledore"
+    },
+    {
+        quote: "It is impossible to manufacture or imitate love",
+        author: "Horace Slughorn"
+    },
+    {
+        quote: "Being different isn't a bad thing. I means that you are brave enough to be yourself.",
+        author: "Luna Lovegood"
+    },
+    {
+        quote: "If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals.",
+        author: "Sirius Black"
+    },
+    {
+        quote: "Never trust anything that can think for itself if you can’t see where it keeps its brain.",
+        author: "Arthur Weasley"
+    },
+    {
+        quote: "Every human life is worth the same, and worth saving.",
+        author: "Kingsley Shacklebolt"
+    },
+    {
+        quote: "Have a biscuit, Potter.",
+        author: "Minerva McGonagall"
+    },
+    {
+        quote: "Happiness can be found even in the darkest of times, if one only remembers to turn on the light.",
+        author: "Albus Dumbledore"
+    },
+    {
+        quote: "Socks are Dobby’s favorite, favorite clothes, sir!",
+        author: "Dobby"
+    }
+];
+
+quotes=quotes.sort(() => Math.random() - 0.5);
+
+
+let i=0;
+
+function randomQote() {
+  
+    if(i<quotes.length){
+    
+    document.getElementById("qoute").innerHTML=quotes[i].quote;
+    document.getElementById("autherqoute").innerHTML=quotes[i].author;
+    i=i+1;
+  
+}
+else{
+    document.getElementById("qoute").innerHTML="No More Quote";
+    document.getElementById("autherqoute").innerHTML=" .... ";
+}
+
+}
